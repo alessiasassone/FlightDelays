b662f7263cb08a105d63b278136342ff8219e04f
alessiasassone <alessiasassone04@gmail.com>
2026-05-12 12:43:37 +0200

    Lezione 11/05

--- database/DAO.py
+++ database/DAO.py
@@ -5,13 +5,21 @@ from model.airport import Airport
 class DAO():
 
     @staticmethod
-    def getAllAirports():
+    def getAllNodes():
         conn = DBConnect.get_connection()
 
         result = []
 
         cursor = conn.cursor(dictionary=True)
-        query = """SELECT * from airports a order by a.AIRPORT asc"""
+        query = """SELECT t.ID, t.IATA_CODE, count(*) as N
+                    FROM (select a.ID , a.IATA_CODE , f.AIRLINE_ID 
+                    from airports a , flights f 
+                    where a.ID  = f.ORIGIN_AIRPORT_ID 
+                    or a.ID = f.DESTINATION_AIRPORT_ID
+                    group BY a.ID , a.IATA_CODE , f.AIRLINE_ID) t 
+                    GROUP BY t.ID, t.IATA_CODE
+                    HAVING N>=5
+                    order by N ASC """
 
         cursor.execute(query)
 
